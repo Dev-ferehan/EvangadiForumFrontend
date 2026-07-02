1085579ead3fdf727692c43c2d84d816065167c8
Dev-ferehan <ahmedferehan7@gmail.com>
2026-07-02 05:35:17 +0300

    i was missing directory link of backend

--- src/axios/axiosConfig.js
+++ src/axios/axiosConfig.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 const axiosConfig=axios.create({
     // baseURL:'http://localhost:5500/api'
-    // baseURL:'https://evangadi-forum-vhr9.onrender.com'
-    baseURL:'https://evangadi-backend-api-d5kx.onrender.com'
+
+    baseURL:'https://evangadi-backend-api-d5kx.onrender.com/api'
 })
 export default axiosConfig
